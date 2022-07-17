6840b090392424dd35097391baf7bd7310a28d96
Aster <galaster@foxmail.com>
2022-07-18 03:10:34 +0800

    🔮 Block wgpu information

--- projects/xcell-gui/src/logger/mod.rs
+++ projects/xcell-gui/src/logger/mod.rs
@@ -1,27 +1,52 @@
-use log::{Log, Metadata, Record, SetLoggerError};
+use std::borrow::Borrow;
+
+use log::{LevelFilter, Log, Metadata, Record, SetLoggerError};
+use regex::Regex;
 
 /// Backend for the log crate facade
 ///
 /// Formats strings then passes them to a chaenel to be displayed in the gui,
 /// this avoids threading issues (logging must be Send+Sync).
-pub struct IcedLogger {}
+pub struct IcedLogger {
+    level: LevelFilter,
+    target_filter: Regex,
+}
+
+impl Default for IcedLogger {
+    fn default() -> Self {
+        Self {
+            level: LevelFilter::Trace,
+            target_filter: Regex::new(r"(wgpu_core|wgpu_hal|naga|iced_wgpu|iced_winit)(::.+)").unwrap(),
+        }
+    }
+}
+impl IcedLogger {
+    fn show_record(&self, record: &Record) -> bool {
+        if self.target_filter.is_match(record.metadata().target()) {
+            return false;
+        }
+        true
+    }
+}
 
 impl IcedLogger {
-    pub fn initialize() -> Result<(), SetLoggerError> {
-        log::set_boxed_logger(Box::new(IcedLogger {}))
-        // log::set_max_level(max_level);
+    pub fn activate(self) -> Result<(), SetLoggerError> {
+        log::set_max_level(self.level);
+        log::set_boxed_logger(Box::new(self))?;
+        Ok(())
     }
 }
 
 impl Log for IcedLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
+        //         metadata.level() <= Level::Info
         println!("{:?} {:?}", metadata.level(), metadata.target());
         true
     }
-
     fn log(&self, record: &Record) {
-        println! {"{:#?}", record};
+        if self.show_record(record) {
+            println! {"{:#?}", record};
+        }
     }
-
     fn flush(&self) {}
 }
--- projects/xcell-gui/src/main.rs
+++ projects/xcell-gui/src/main.rs
@@ -1,10 +1,60 @@
+#![feature(once_cell)]
+
+use crate::logger::IcedLogger;
+use iced::{
+    widget::{button, column, text},
+    Alignment, Element, Sandbox, Settings,
+};
+
 mod errors;
 mod logger;
 
-pub use errors::{Error, Result};
-use logger::IcedLogger;
-
-fn main() {
-    IcedLogger::initialize().unwrap();
+pub fn main() -> iced::Result {
+    IcedLogger::default().activate().unwrap();
     log::info!("Hello, world!");
+    Counter::run(Settings::default())
+}
+
+struct Counter {
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        column![
+            button("Increment").on_press(Message::IncrementPressed),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::DecrementPressed)
+        ]
+        .padding(20)
+        .align_items(Alignment::Center)
+        .into()
+    }
 }
